061d6ffff34dcabc6df70573f195cfe87e332ad1
JNightcrawler <ddepakr@gmail.com>
2022-12-09 22:16:20 +0530

    controller

--- src/main/java/com/speedtest/controller/SpeedTestController.java
+++ src/main/java/com/speedtest/controller/SpeedTestController.java
@@ -1,2 +1,13 @@
-package com.speedtest.controller;public class SpeedTestController {
+package com.speedtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpeedTestController {
+
+    @GetMapping("/getValue")
+    public String getValue(){
+        return "Hello";
+    }
 }
